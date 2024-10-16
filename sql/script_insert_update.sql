428255ce19bac2d0b2c068777b0a1b82213404a7
jcromani84 <julio.c.romani@gmail.com>
2024-10-16 16:01:11 -0500

    Segundo commit

--- sql/script_insert_update.sql
+++ sql/script_insert_update.sql
@@ -1,14 +1,14 @@
 BEGIN
-    sp_InsertarPersona('Juan', 'Pérez', '123456789', 'CC', 'juan.perez@example.com', 1500.00, 'ACTIVO');
-    sp_InsertarPersona('Ana', 'Gómez', '987654321', 'TI', 'ana.gomez@example.com', 2000.00, 'ACTIVO');
-    sp_InsertarPersona('Luis', 'Martínez', '456789123', 'NIT', 'luis.martinez@example.com', 2500.00, 'INACTIVO');
-    sp_InsertarPersona('Marta', 'Rodríguez', '321654987', 'PAS', 'marta.rodriguez@example.com', 3000.00, 'ACTIVO');
-    sp_InsertarPersona('Carlos', 'Sánchez', '654321789', 'CC', 'carlos.sanchez@example.com', 1800.00, 'INACTIVO');
+    sp_InsertarPersona('Juan', 'Pérez', '123456789', 'CC', 'juan.perez@correo.com', 1500.00, 'ACTIVO');
+    sp_InsertarPersona('Ana', 'Gómez', '987654321', 'TI', 'ana.gomez@correo.com', 2000.00, 'ACTIVO');
+    sp_InsertarPersona('Luis', 'Martínez', '456789123', 'NIT', 'luis.martinez@correo.com', 2500.00, 'INACTIVO');
+    sp_InsertarPersona('Marta', 'Rodríguez', '321654987', 'PAS', 'marta.rodriguez@correo.com', 3000.00, 'ACTIVO');
+    sp_InsertarPersona('Carlos', 'Sánchez', '654321789', 'CC', 'carlos.sanchez@correo.com', 1800.00, 'INACTIVO');
 END;
 
 
 BEGIN
-    sp_ActualizarPersona(1, 'Juan Carlos', 'Pérez', '123456789', 'CC', 'juan.perez@nuevo.com', 1600.00, 'ACTIVO');
-    sp_ActualizarPersona(2, 'Ana María', 'Gómez', '987654321', 'TI', 'ana.maria.gomez@example.com', 2100.00, 'ACTIVO');
-    sp_ActualizarPersona(3, 'Luis Fernando', 'Martínez', '456789123', 'NIT', 'luis.fernando.martinez@example.com', 2600.00, 'INACTIVO');
+    sp_ActualizarPersona(1, 'Juan Carlos', 'Pérez', '123456789', 'CC', 'juan.perez@correonuevo.com', 1600.00, 'ACTIVO');
+    sp_ActualizarPersona(2, 'Ana María', 'Gómez', '987654321', 'TI', 'ana.maria.gomez@correo.com', 2100.00, 'ACTIVO');
+    sp_ActualizarPersona(3, 'Luis Fernando', 'Martínez', '456789123', 'NIT', 'luis.fernando.martinez@correo.com', 2600.00, 'INACTIVO');
 END;
